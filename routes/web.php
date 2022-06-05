03e69157c31bf9cc4a92db6483c84574d76b01c1
HUMAUN KABIR <kabircse2018@gmail.com>
2022-06-05 18:09:05 +0600

    ok

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,3 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::get('/test', [App\Http\Controllers\HomeController::class, 'index'])->name('test');
